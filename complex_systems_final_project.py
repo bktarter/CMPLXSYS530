a8940bec42d2b5208f24ad0faab428efe71e5481
bktarter <103272607+bktarter@users.noreply.github.com>
2022-04-08 21:09:16 -0400

    Combine Code v1
    combine henglin code with main code

--- complex_systems_final_project.py
+++ complex_systems_final_project.py
@@ -1,3 +1,4 @@
+from pylab import *
 from IPython import display
 import networkx as nx
 import time
@@ -8,15 +9,15 @@ import math
 import matplotlib.pyplot as plt
 
 # pick = which tracker
-pick = 1
+pick = 2
 
 def initialize(trackerSheet):
-  global g, nextg, prices, maxCommodityYield, pos
+  global g, nextg, prices, maxCommodityYield, pos, test, df
 
   if trackerSheet == 1:
-    df = pd.read_excel('CMPLXSYS530\CaliforniaAlmondTracker.xlsx', usecols='A:AA')
+    df = pd.read_excel('CaliforniaAlmondTracker.xlsx', usecols='A:AA')
   if trackerSheet == 2:
-    df = pd.read_excel('CMPLXSYS530\CaliforniaCountyList.xlsx',usecols="A:AA")
+    df = pd.read_excel('CaliforniaCountyList.xlsx',usecols="A:AA")
   
   g = nx.Graph()
 
@@ -63,19 +64,23 @@ def initialize(trackerSheet):
   maxCommodityYield['maxgrapeYield'] = 0
   maxCommodityYield['maxpistachioYield'] = 0
   maxCommodityYield['maxcattleYield'] = 0
-  maxCommodityYield['maxlettuceYeild'] = 0
+  maxCommodityYield['maxlettuceYield'] = 0
   maxCommodityYield['maxstrawberryYield'] = 0
   maxCommodityYield['maxtomatoYield'] = 0
   maxCommodityYield['maxwalnutYield'] = 0
   
+  #crop yield should be tons/acre
+  
   #Capacity is determined by Node with Max Yield in any Commodity
 
-  fireProb = df['Observed Fire Prob']
-  #fireProb = df['Predicted Fire Prob']
+  #fireProb = df['Observed Fire Prob']
+  fireProb = df['Predicted Fire Prob']
+  fireprob = np.array(fireProb) / np.sum(np.array(fireProb))
   
   if trackerSheet ==1:
      attributes = {i: {'onFire':0 ,
                       'firstFire':False,
+                      'fireProb':fireProb[i],
                       'almondYield':df['Almond Yield (tons/ac)'][i],
                       } for i in g.nodes()}
 
@@ -83,8 +88,8 @@ def initialize(trackerSheet):
     attributes = {i: {'onFire':0 ,
                       'firstFire':False,
                       'fireProb':fireProb[i],
-                      'dairyYield':df['Dairy Yield'][i],
-                      'almondYield':df['Almond Yield'][i],
+                      #'dairyYield':df['Dairy Yield'][i], # removed from sheet
+                      'almondYield':df['Almond Yield'][i], 
                       'grapeYield':df['Grape Yield'][i],
                       'pistachioYield':df['Pistachio Yield'][i],
                       'cattleYield':df['Cattle Yield'][i], 
@@ -98,32 +103,54 @@ def initialize(trackerSheet):
   #set maxCommodityYields
   for node in range(len(Nodes)):
        for attributes in g.nodes[node]:
-         if attributes != 'pos' and attributes != 'onFire' and attributes != 'firstFire':
+         if attributes != 'pos' and attributes != 'onFire' and attributes != 'firstFire' and attributes != 'fireProb' and attributes != 'rebornDuration':
               att = 'max' + attributes
               if g.nodes[node][attributes] > maxCommodityYield[att]:
                 maxCommodityYield[att] = g.nodes[node][attributes]
   
-
+  #where to start first fire
+  initialFire_index = 10
+  g.nodes[initialFire_index]['onFire'] = 1
+  g.nodes[initialFire_index]['firstFire'] = True
+  g.nodes[initialFire_index]['fireDuration'] = 2
+  g.nodes[initialFire_index]['rebornDuration'] = 2 + 60
+  for attributes in g.nodes[initialFire_index]:
+    if attributes != 'pos' and attributes != 'onFire' and attributes != 'firstFire' and attributes != 'fireDuration' and attributes != 'rebornDuration':
+      g.nodes[initialFire_index][attributes] = 0
   
-
 def update():
-    global g, nextg, prices, maxCommodityYield
+    global g, nextg, pos, test,df
     
     # Update network model
     curprev = 0
     nextg = g.copy()
-    nextg.pos = g.pos
+    #nextg.pos = g.pos
 
     for a in g.nodes:
-      if g.nodes[a]['onFire'] == False : #able to catch on fire
-        nextg.nodes[a]['OnFire'] = False
-        
-      for b in g.neighbors(a):
-        if g.nodes[b]['onFire'] == True and g.nodes[b]['firstFire'] == True: # if neighbor b is on Fire
-          nextg.nodes[a]['onFire']  = 1
+      if g.nodes[a]['firstFire'] == True and g.nodes[a]['fireDuration'] == 1: #able to catch on fire
+        nextg.nodes[a]['firstFire'] == False
+        for b in g.neighbors(a):
+          nextg.nodes[b]['onFire']  = 1
+          nextg.nodes[b]['fireDuration']  = 2
+          nextg.nodes[b]['rebornDuration']  = 60 + 2
+
+      if g.nodes[a]['onFire'] == 1 and g.nodes[a]['fireDuration'] >= 1:
+        nextg.nodes[a]['fireDuration']  = g.nodes[a]['fireDuration'] - 1
+
+      if g.nodes[a]['fireDuration'] == 0:
+        nextg.nodes[a]['onFire'] = 0
+
+      if g.nodes[a]['rebornDuration'] > 0:
+        nextg.nodes[a]['rebornDuration'] = g.nodes[a]['rebornDuration'] - 1
+        if g.nodes[a]['rebornDuration'] == 1:
+          #nextg.nodes[a]['cattleYield'] = df['Cattle Yield'][a]
+          for attributes in g.nodes[a]:
+            if attributes != 'onFire' and attributes != 'firstFire' and attributes != 'fireDuration' and attributes != 'rebornDuration':
+              nextg.nodes[a][attributes] = df[attributes][a]
       
     g = nextg.copy()
-    g.pos = nextg.pos
+    test.append(g.nodes[12]['cattleYield'])
+    #g.pos = nextg.pos
 
 def observe():
     global g, nextg, prices, maxCommodityYield
@@ -136,5 +163,19 @@ def observe():
     plt.show()
 
 initialize(pick)
-nx.draw(g,pos, node_color = 'blue')
+update()
+observe()
+
+test = [] 
+
+initialize(pick)
+observe()
 plt.show()
+for i in range(70):
+  update()
+  observe()
+  plt.show() #show every step
+  # if i%10==0: plt.show() #show the plot every 10 steps
+
+plot(test)
+show()
